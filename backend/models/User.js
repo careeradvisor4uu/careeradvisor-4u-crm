1f32674954539242d4a53e446b218b1eb803013a
careeradvisor4uu <careeradvisor4uu@gmail.com>
2026-03-31 14:32:46 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,8 +3,10 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true }, // In production, we would hash this
-  role: { type: String, enum: ["admin", "caller"], default: "caller" }
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "caller"], default: "caller" },
+  resetToken: String,
+  resetTokenExpiry: Date
 });
 
 module.exports = mongoose.model("User", UserSchema);
