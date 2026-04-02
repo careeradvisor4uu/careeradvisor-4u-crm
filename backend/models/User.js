4f9255bf7077293701cfffbe18fea55e49435061
careeradvisor4uu <careeradvisor4uu@gmail.com>
2026-04-02 06:41:30 +0530

    fix: hash passwords on save

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -7,6 +8,12 @@ const UserSchema = new mongoose.Schema({
   role: { type: String, enum: ["admin", "caller"], default: "caller" },
   resetToken: String,
   resetTokenExpiry: Date
+});
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
